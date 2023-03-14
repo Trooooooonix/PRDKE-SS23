c68f0ce3e49c9dc9f425f51b8c86da7fdf9cb1ce
Trooooooonix <andi_h@gmx.net>
2023-03-14 13:06:21 +0100

    Revert "Login Logout works"
    This reverts commit 742402bc74eeab5d52b980b9d4d57e5f71af2219.

--- projectDirectory/app/routes.py
+++ projectDirectory/app/routes.py
@@ -1,14 +1,9 @@
 from app import app
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from werkzeug.urls import url_parse
-
 
 @app.route("/")
 @app.route("/index")
-@login_required
 def home_site():
     user = {'username': 'Andi'}
     posts = [
@@ -40,29 +35,9 @@ def overview():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # if user is already logged in
-    if current_user.is_authenticated:
-        return redirect(url_for('home_site'))
-
-    #validates login data
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-
-        # checks login data
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password!')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-
-        # if necessary login is tried to avoided
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home_site')
-        return redirect(next_page)
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('home_site'))
     return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
