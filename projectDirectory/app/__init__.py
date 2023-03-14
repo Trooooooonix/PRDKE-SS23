c85bddb9a05eb5c435860a4559296ba60a825cb1
Trooooooonix <andi_h@gmx.net>
2023-03-14 13:38:45 +0100

    savety-commit

--- projectDirectory/app/__init__.py
+++ projectDirectory/app/__init__.py
@@ -2,11 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# login
+login = LoginManager(app)
+# makes the login necessary
+login.login_view = 'login'
 
 
 from app import routes, models
